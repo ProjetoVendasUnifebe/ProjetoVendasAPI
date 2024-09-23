a4407e35ab9ced6cda7c92c4322e0102a560a3d7
Nome-Usuario-Git <emailcorporativo@havan.com.br>
2024-09-23 15:39:42 -0300

    Ajustando Arquitetura

---
+++ Vendas.API/Controllers/VendasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vendas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendasController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Teste";
+        }
+    }
+}

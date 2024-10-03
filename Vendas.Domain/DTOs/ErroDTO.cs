de114ea34cda2fc8a72907e54a910b348bb51b70
Luis F. R. Barnabé <124844637+LuisFelipeRichterBarnabe@users.noreply.github.com>
2024-10-03 07:59:53 -0300

    Realizando a conexão com o banco

---
+++ Vendas.Domain/DTOs/ErroDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vendas.Domain.DTOs
+{
+    public class ErroDTO
+    {
+        public string TipoErro { get; set; }
+        public string Descricao { get; set; }
+
+        public ErroDTO(string tipoErro, string descricao)
+        {
+            TipoErro = tipoErro;
+            Descricao = descricao;
+        }
+    }
+}
---
+++ Vendas.Infra/Mappings/UsuarioMap.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vendas.Domain.Entities;
+
+namespace Vendas.Infra.Mappings
+{
+    public class UsuarioMap : IEntityTypeConfiguration<UsuarioModel>
+    {
+        public void Configure(EntityTypeBuilder<UsuarioModel> builder)
+        {
+            builder.ToTable("usuario", "comercialize");
+
+            builder.HasKey(x => x.IdUsuario);
+
+            builder.Property(x => x.IdUsuario).HasColumnName("idUsuario").HasColumnType("bigint").IsRequired();
+            builder.Property(x => x.NomeUsuario).HasColumnName("nomeUsuario").HasColumnType("varchar(60)").IsRequired();
+            builder.Property(x => x.Senha).HasColumnName("senha").HasColumnType("varchar(60)").IsRequired();
+        }
+    }
+}

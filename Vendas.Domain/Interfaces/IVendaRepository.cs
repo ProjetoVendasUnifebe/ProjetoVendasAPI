e835e88a28227461cdc77fec6571bbdfb465d3a6
Leo Tachini <leo1tachini@gmail.com>
2024-10-04 21:11:48 -0300

    Criando Repository e interface de Venda

---
+++ Vendas.Domain/Interfaces/IVendaRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vendas.Domain.Entities;
+
+namespace Vendas.Domain.Interfaces
+{
+    public interface IVendaRepository
+    {
+        List<VendaModel> BuscarVendas();
+    }
+}
